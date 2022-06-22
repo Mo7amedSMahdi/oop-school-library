d4711ef57d7749a9344320daf57606ec303d2795
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-22 13:03:54 +0300

    set up associations
    - Added Book class.
    - Added ClassRoom class.

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require './nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
