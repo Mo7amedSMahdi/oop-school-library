d5e6940e95bec6512285f73905b57c56d314e8f3
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-28 19:38:57 +0300

    fix robucop offenses

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,15 +1,15 @@
-require "./teacher"
+require './teacher'
 
 describe Teacher do
-  context "testing Teacher class" do
-    teacher = Teacher.new(name: "David Smith", age: 20, id: 1, parent_permission: true, specialization: "History")
+  context 'testing Teacher class' do
+    teacher = Teacher.new(name: 'David Smith', age: 20, id: 1, parent_permission: true, specialization: 'History')
 
-    it "should have a name" do
-      expect(teacher.name).to eq("David Smith")
+    it 'should have a name' do
+      expect(teacher.name).to eq('David Smith')
     end
 
-    it "Should specialize in History" do
-      expect(teacher.specialization).to eq("History")
+    it 'Should specialize in History' do
+      expect(teacher.specialization).to eq('History')
     end
   end
 end
