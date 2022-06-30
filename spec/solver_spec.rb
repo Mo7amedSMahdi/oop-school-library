ad4a954ce9e216d8164768add4a98f00f788c138
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-30 13:05:45 +0300

    added tests for reverse methods

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,7 +2,7 @@ require './solver'
 
 describe Solver do
     solver = Solver.new
-    context 'testing solver class factorial methods' do
+    context 'Testing solver class => factorial methods' do
         it 'Returns an instance of the solver class' do
       @our_factorial = solver.factorial(2)
       expect(@our_factorial).to eq(2)
@@ -13,4 +13,16 @@ describe Solver do
       expect(@our_factorial).to eq 'Can not have factorial of a negative number'
     end
     end
+
+     context 'Testing solver class => reverse' do
+    it 'Returns an instance of the solver class' do
+      @reversed_word = solver.reverse('hello')
+      expect(@reversed_word).to eq('olleh')
+    end
+
+    it 'Returns an instance of the solver class' do
+      @reversed_word = solver.reverse('eye')
+      expect(@reversed_word).to eq('eye')
+    end
+  end
 end
