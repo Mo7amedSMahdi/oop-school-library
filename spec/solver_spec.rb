8a61c716105d31c739047997e1939c588b008917
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-06-30 13:07:33 +0300

    added test for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -25,4 +25,27 @@ describe Solver do
       expect(@reversed_word).to eq('eye')
     end
   end
+
+  context 'Testing solver class => fizzbuzz' do
+    it 'When N is divisible by 3, returns fizz' do
+      calc = Solver.new
+      @fizzbuzz = calc.fizzbuzz(3)
+      expect(@fizzbuzz).to eq('fizz')
+    end
+
+    it 'When N is divisible by 5, returns buzz' do
+      @fizzbuzz = solver.fizzbuzz(5)
+      expect(@fizzbuzz).to eq('buzz')
+    end
+
+    it 'When N is divisible by 3 and 5, returns fizzbuzz' do
+      @fizzbuzz = solver.fizzbuzz(15)
+      expect(@fizzbuzz).to eq('fizzbuzz')
+    end
+
+    it 'Any other case, return N as a string' do
+      @fizzbuzz = solver.fizzbuzz(4)
+      expect(@fizzbuzz).to eq('4')
+    end
+  end
 end
